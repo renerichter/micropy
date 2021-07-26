minor fixes > fix split_nd to return at least an image of tile_size > add err_bar to stack_plot > fix masking and standard parameters for shannon_entropy

MicroPy/filters.py
@@ -4,7 +4,7 @@
 	@author René Lachmann
 	@email herr.rene.richter@gmail.com
 	@create date 2019-11-25 10:26:14
-	@modify date 2021-06-22 14:04:37
+	@modify date 2021-07-23 12:37:41
 	@desc The Filters are build such that they assume to receive an nD-stack, but they only operate in a 2D-manner (meaning: interpreting the stack as a (n-2)D series of 2D-images). Further, they assume that the last two dimensions (-2,-1) are the image-dimensions. The others are just for stacking.
 
 ---------------------------------------------------------------------------------------------------
@@ -287,7 +287,7 @@ def spf_kristans_bayes_spectral_entropy(im, faxes=(-2, -1), tile_exp=3, klim=6, 
     return res, [nom, denom]
 
 
-def spf_dct_normalized_shannon_entropy(im, faxes=(-2, -1), klim=100, krel=1e-2,**kwargs):
+def spf_dct_normalized_shannon_entropy(im, faxes=(-2, -1), klim=100, krel=1e-2, klim_max=None, **kwargs):
     """Calculates the normalized shannon entropy.
 
     Parameters
@@ -310,32 +310,40 @@ def spf_dct_normalized_shannon_entropy(im, faxes=(-2, -1), klim=100, krel=1e-2,*
 
     # make sure for OTF-support
     if klim is None:
-        mask = np.copy(imDCT)
-        mask[mask<np.max(mask)*krel]=0
-        mask[mask>np.max(mask)*krel]=1
-        
-        if imDCT.ndim>3:
-            mask_pad = tuple([(0,0),]*(imDCT.ndim-3)+[(4,4),(4,4),(4,4)])
-            mask_struct = np.ones( [1,]*(imDCT.ndim-3)+[3,3,3])
-            sdim=-3
-        elif imDCT.ndim==3:
-            mask_pad = ((0,0),(4,4),(4,4))
-            mask_struct = np.ones([1,3,3])
-            sdim=-3
+        if klim_max is None:
+            mask = np.copy(imDCT)
         else:
-            mask_pad = ((4,4),(4,4))
-            mask_struct = np.ones([3,3])
-            sdim=-2
-        mask = np.pad(mask, mask_pad, mode='constant', constant_values=0)
-        mask = binary_closing(mask, structure=mask_struct ,iterations=3)
-        mask = nip.extract(img=mask,ROIsize=imDCT.shape)
+            mask = np.copy(imDCT[klim_max])
+        mask[mask < np.max(mask)*krel] = 0
+        mask[mask > np.max(mask)*krel] = 1
 
+        if mask.ndim > 3:
+            mask_pad = tuple([(0, 0), ]*(imDCT.ndim-3)+[(4, 4), (4, 4), (4, 4)])
+            mask_struct = np.ones([1, ]*(imDCT.ndim-3)+[3, 3, 3])
+            sdim = -3
+        elif mask.ndim == 3:
+            mask_pad = ((0, 0), (4, 4), (4, 4))
+            mask_struct = np.ones([1, 3, 3])
+            sdim = -3
+        else:
+            mask_pad = ((4, 4), (4, 4))
+            mask_struct = np.ones([3, 3])
+            sdim = -2
+        mask = np.pad(mask, mask_pad, mode='constant', constant_values=0)
+        mask = binary_closing(mask, structure=mask_struct, iterations=3)
+        if mask.ndim > 2:
+            mask = np.max(mask, axis=tuple(np.arange(0, mask.ndim-2)))
+        mask = nip.extract(img=mask, ROIsize=imDCT.shape)
         # assume spherical support and calculate radius from area per slice
-        klim_sqr=np.floor((4*np.sum(mask,axis=(-2,-1))/np.pi)).astype('int32')
+        klim_sqr = np.floor((4*np.sum(mask, axis=(-2, -1))/np.pi)).astype('int32')
     else:
-        mask = nip.rr(en_el, placement='corner') < klim
+        #mask_shape = imDCT.shape[-3:] if imDCT.ndim >= 3 else imDCT.shape[-2:]
+        xmask = abs(nip.xx(imDCT.shape, placement='corner'))
+        ymask = abs(nip.yy(imDCT.shape, placement='corner'))
+        zmask = abs(nip.zz(imDCT.shape, placement='center'))
+        mask = (np.sqrt(xmask*xmask+ymask*ymask+zmask*zmask) < klim).astype('bool')
         klim_sqr = klim*klim
-    
+
     im_res = (en_el * np.log2(en_el, where=en_el != 0, out=np.zeros(en_el.shape)))
 
     # calculate norm
@@ -532,8 +540,8 @@ class filters():
         'shannon_entropy':  [spf_dct_normalized_shannon_entropy,    ((0, 0), (0, 0)),   True, [0.7, 0.7, 0.7, 1], [(-2, -1), (-2, -1)], 'Shannon Entropy'],
     }
     _filters_special_params_ = {
-        'kristans_entropy': {'tile_exp': 3, 'klim': 6, },
-        'shannon_entropy':  {'klim': None, 'krel':1e-2},
+        'kristans_entropy': {'tile_exp': 4, 'klim': 2, },
+        'shannon_entropy':  {'klim': None, 'krel': 1e-2},
     }
 
     _colors_ = []

MicroPy/inout.py
@@ -795,7 +795,7 @@ def print_stack2subplot(imstack, plt_raster=[4, 4], plt_format=[8, 6], title=Non
 #
 
 
-def stack2plot(x, ystack, refs=None, title=None, xlabel=None, ylabel=None, colors=None, mmarker='', mlinestyle='-', mlinewidth=None, legend=[1, 1.05], xlims=None, ylims=None, figsize=(8, 8), show_plot=True, ptight=True, ax=None, nbrs=True, nbrs_color=[1, 1, 1], nbrs_size=None, nbrs_text=None):
+def stack2plot(x, ystack, refs=None, title=None, xlabel=None, ylabel=None, colors=None, mmarker='', mlinestyle='-', mlinewidth=None, legend=[1, 1.05], xlims=None, ylims=None, figsize=(8, 8), show_plot=True, ptight=True, ax=None, nbrs=True, nbrs_color=[1, 1, 1], nbrs_size=None, nbrs_text=None, err_bar=None, err_capsize=3):
     '''
     Prints a 1d-"ystack" into 1 plot and assigns legends + titles.
     '''
@@ -817,8 +817,14 @@ def stack2plot(x, ystack, refs=None, title=None, xlabel=None, ylabel=None, color
         xlabel = 'Pixel' if xlabel is None else xlabel
         ylabel = 'Pixel' if ylabel is None else ylabel
         title = datetime.now().strftime("%Y%M%D") if title is None else title
-        line, = ax.plot(x, ystack[m], label=label, color=colorse,
-                        marker=mmarker, linestyle=mlinestyle)
+        if err_bar is None:
+            line, = ax.plot(x, ystack[m], label=label, color=colorse,
+                            marker=mmarker, linestyle=mlinestyle)
+            line.set_clip_on(False)
+        else:
+            line = ax.errorbar(x, ystack[m], err_bar[m], label=label, color=colorse,
+                               marker=mmarker, linestyle=mlinestyle, capsize=err_capsize)
+            line.set_clip_on(False)
         if not mlinewidth is None:
             line.set_linewidth(mlinewidth[m])
         line.set_antialiased(False)
@@ -833,6 +839,7 @@ def stack2plot(x, ystack, refs=None, title=None, xlabel=None, ylabel=None, color
     # axis limits
     xlims = [np.min(x), np.max(x)] if xlims is None else xlims
     ylims = [np.min(ystack), np.max(ystack)] if ylims is None else ylims
+    ylims_dist = ylims[1]-ylims[0]
     ax.set_xlim(xlims)
     ax.set_ylim(ylims)
 
@@ -841,7 +848,8 @@ def stack2plot(x, ystack, refs=None, title=None, xlabel=None, ylabel=None, color
         nbrs_psize = text_size_factor * 0.05 if nbrs_size is None else text_size_factor*nbrs_size
         x_offset = np.mean(ax.get_xticks()[:2])
         # ax.get_yticks()[-1] - 4*nbrs_psize/text_size_factor*(ylims[1]-ylims[0])
-        y_offset = ax.get_yticks()[-2]
+        #y_offset = ax.get_yticks()[-2]
+        y_offset = ylims[1]-ylims_dist/5.0
         nbrs_text = chr(97+np.random.randint(26))+')' if nbrs_text is None else nbrs_text
         ax.text(x_offset, y_offset, nbrs_text, fontsize=nbrs_psize,
                 color=nbrs_color, fontname='Helvetica', weight='normal')

MicroPy/utility.py
@@ -4,7 +4,7 @@
 	@author René Lachmann
 	@email herr.rene.richter@gmail.com
 	@create date 2019 11:53:25
-	@modify date 2021-07-06 15:07:41
+	@modify date 2021-07-20 14:39:52
 	@desc Utility package
 
 ---------------------------------------------------------------------------------------------------
@@ -1425,8 +1425,11 @@ def split_nd(im, tile_sizes=[8, 8], split_axes=[-1, -2]):
 
     imshape = np.array([im.shape[m] for m in split_axes])
     im_multiples = np.floor(imshape // tile_sizes).astype('uint8')
+    im_multiples = im_multiples if np.prod(
+        im_multiples) > 0 else np.ones(len(im_multiples)).astype('int')
 
-    if not np.sum(np.mod(imshape, tile_sizes)) == 0:
+    # make image rect (as multiple of tile_sizes) and pad zeros
+    if not np.sum(np.mod(imshape, tile_sizes)) == 0:  # was np.mod(imshape, tile_sizes)
         imshapeh = list(im.shape)
         for m, sa in enumerate(split_axes):
             imshapeh[sa] = im_multiples[m]*tile_sizes[m]
@@ -1696,7 +1699,7 @@ def time_me(fcall, repeats=1000, averages=100):
     time_me_loop
     """
     time_repeats = timeit.repeat(
-        lambda: fcall, number=averages, repeat=repeats)
+        fcall, number=averages, repeat=repeats)
     time_repeats = np.array(time_repeats)/averages
     time_stats = time_me_stats(time_repeats)
 