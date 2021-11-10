> unmixing extent svd_stat and fix calls > add noise-normalization > fix subslice_arbitrary > add midIndexList

MicroPy/processingISM.py
@@ -178,21 +178,21 @@ def test_otf_symmetry(otf):
     nip.v5(nip.cat((psfh3, psfc, psfh2)))
 
 
-def pinv_unmix(a, rcond=1e-15, svdnum=None, eps_reg=0, use_own=True):
+def pinv_unmix(a, svdlim=1e-15, svdnum=None, eps_reg=0, eps_reg_rel=0, use_own=True):
     """
     Functions as a wrapper and a regularized version of the np.linalg.pinv.
 
     :PARAM:
     =======
     :a:         (ARRAY/IMAGE) Array to invert
-    :rcond:     (FLOAT) relative cutoff
-    :svdlim:    (INT) maximum nbr of SVD-values to keep
+    :svdlim:     (FLOAT) relative cutoff
+    :svdnum:    (INT) maximum nbr of SVD-values to keep
     :eps:       (FLOAT) regularizer
 
     """
     # use original pinv?
     if not use_own:
-        res = np.linalg.pinv(a=a, rcond=rcond)
+        res = np.linalg.pinv(a=a, rcond=svdlim)
         outdict = {}
 
     else:
@@ -200,55 +200,61 @@ def pinv_unmix(a, rcond=1e-15, svdnum=None, eps_reg=0, use_own=True):
         u, s, vt = np.linalg.svd(a, full_matrices=False)
         s_full = np.copy(s)
 
-        # debug-hook
-        # if len(s) > 20:
-        #    print("stop")
-
         # discard small singular values; regularize singular-values if wanted -> note: eps_reg is 0 on default
-        if svdnum == None:
-            cutoff = np.array(rcond)[..., np.newaxis] * \
-                np.amax(s, axis=-1, keepdims=True)
-            large = s > cutoff
+        if svdnum is None:
+            if svdlim is None:
+                large = np.ones(s.shape, dtype=bool)
+            else:
+                cutoff = np.array(svdlim)[..., np.newaxis] * \
+                    np.amax(s, axis=-1, keepdims=True)
+                large = s > cutoff
 
-            # Tikhonov regularization analogue to idea 1/(OTF+eps) where eps becomes dominant when OTF<eps, especially reduces to 1/eps when OTF<<eps
-            s = np.divide(s, s*s+eps_reg, where=large,
-                          out=s) if eps_reg else np.divide(1, s, where=large, out=s)
-            s[~large] = 0
         else:
-            cutoff = s[:svdnum] if svdnum < len(s) else s
-            cutoff = cutoff/(cutoff*cutoff+eps_reg) if eps_reg else 1/cutoff
+            #cutoff = s[:svdnum] if svdnum < len(s) else s
+            large = s > s[svdnum] if svdnum < len(s) else np.ones(s.shape, dtype=bool)
 
-            s = np.zeros(s.shape)
-            s[:len(cutoff)] = cutoff
+        if eps_reg == 0:
+            eps_reg = s[0]*eps_reg_rel
+
+        # Tikhonov regularization analogue to idea 1/(OTF+eps) where eps becomes dominant when OTF<eps, especially reduces to 1/eps when OTF<<eps
+        s = np.divide(s, s*s+eps_reg, where=large,
+                      out=s) if eps_reg else np.divide(1, s, where=large, out=s)
+        s[~large] = 0
 
         #res = np.matmul(np.transpose(vt), np.multiply(s[..., np.newaxis], np.transpose(u)))
         res = np.transpose(np.dot(u*s, vt))
 
         # return results
-        outdict = {'u': u, 's_full': s_full, 's': s, 'vt': vt}
+        outdict = {'u': u, 's_full': s_full, 's': s, 'vt': vt, 'eps_reg': eps_reg}
 
     # done?
     return res, outdict
 
 
-def unmix_svd_stat(svd_range, sing_vals, kk, jj):
-    if (sing_vals[0]/sing_vals[-1]) > svd_range['biggest_ratio']['ratio']:
-        svd_range['biggest_ratio']['ratio'] = sing_vals[0]/sing_vals[-1]
-        svd_range['biggest_ratio']['max'] = sing_vals[0]
-        svd_range['biggest_ratio']['min'] = sing_vals[-1]
-        svd_range['biggest_ratio']['s_list'] = sing_vals
-        svd_range['biggest_ratio']['kk'] = kk
-        svd_range['biggest_ratio']['jj'] = jj
-    if (sing_vals[-1] < svd_range['smallest_sv']['sv']):
-        svd_range['smallest_sv']['sv'] = sing_vals[-1]
-        svd_range['smallest_sv']['s_list'] = sing_vals
-        svd_range['smallest_sv']['kk'] = kk
-        svd_range['smallest_sv']['jj'] = jj
-    if (sing_vals[0] > svd_range['biggest_sv']['sv']):
-        svd_range['biggest_sv']['sv'] = sing_vals[0]
-        svd_range['biggest_sv']['s_list'] = sing_vals
-        svd_range['biggest_sv']['kk'] = kk
-        svd_range['biggest_sv']['jj'] = jj
+def unmix_svd_stat(svd_range, sing_vals, eps_reg, kk, jj):
+    s = svd_range['biggest_ratio']
+    if (sing_vals[0]/sing_vals[-1]) > s['ratio']:
+        s['ratio'] = sing_vals[0]/sing_vals[-1]
+        s['max'] = sing_vals[0]
+        s['min'] = sing_vals[-1]
+        s['s_list'] = sing_vals
+        s['eps_reg'] = eps_reg
+        s['kk'] = kk
+        s['jj'] = jj
+    s = svd_range['smallest_sv']
+    if (sing_vals[-1] < s['sv']):
+        s['sv'] = sing_vals[-1]
+        s['s_list'] = sing_vals
+        s['eps_reg'] = eps_reg
+        s['kk'] = kk
+        s['jj'] = jj
+    s = svd_range['biggest_sv']
+    if (sing_vals[0] > s['sv']):
+        s['sv'] = sing_vals[0]
+        s['s_list'] = sing_vals
+        s['eps_reg'] = eps_reg
+        s['kk'] = kk
+        s['jj'] = jj
     return svd_range
 
 
@@ -256,16 +262,16 @@ def unmix_svdstat_pp(svd_range):
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\tSVD-Statistics\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     a = svd_range['biggest_ratio']
     print(
-        f"Biggest Ratio:\n >>>> ratio={a['ratio']:.2e}, max={a['max']:.2e}, min={a['min']:.2e}, len(s)={len(a['s_list'])}, [kx,ky]=[{a['kk']},{a['jj']}].\n >>>> sv_list={format_list(a['s_list'],'.3e')}.")
+        f"Biggest Ratio:\n >>>> ratio={a['ratio']:.2e}, max={a['max']:.2e}, min={a['min']:.2e}, eps_reg={a['eps_reg']:.2e}, len(s)={len(a['s_list'])}, [kx,ky]=[{a['kk']},{a['jj']}].\n >>>> sv_list={format_list(a['s_list'],'.3e')}.")
     a = svd_range['biggest_sv']
     print(
-        f"Biggest Singular Value:\n >>>> λ={a['sv']:.2e}, len(s)={len(a['s_list'])}, [kx,ky]=[{a['kk']},{a['jj']}].\n >>>> sv_list={format_list(a['s_list'],'.3e')}.")
+        f"Biggest Singular Value:\n >>>> λ={a['sv']:.2e}, eps_reg={a['eps_reg']:.2e}, len(s)={len(a['s_list'])}, [kx,ky]=[{a['kk']},{a['jj']}].\n >>>> sv_list={format_list(a['s_list'],'.3e')}.")
     a = svd_range['smallest_sv']
     print(
-        f"Smallest Singular Value:\n >>>> λ={a['sv']:.2e}, len(s)={len(a['s_list'])}, [kx,ky]=[{a['kk']},{a['jj']}].\n >>>> sv_list={format_list(a['s_list'],'.3e')}.")
+        f"Smallest Singular Value:\n >>>> λ={a['sv']:.2e}, eps_reg={a['eps_reg']:.2e}, len(s)={len(a['s_list'])}, [kx,ky]=[{a['kk']},{a['jj']}].\n >>>> sv_list={format_list(a['s_list'],'.3e')}.")
 
 
-def unmix_matrix(otf, mode='rft', eps_mask=5e-4, eps_reg=1e-17, svdlim=1e-15, svdnum=None, hermitian=False, use_own=True, closing=None, center_pinhole=None, verbose=True, svd_stat=False):
+def unmix_matrix(otf, mode='rft', eps_mask=5e-4, eps_reg=1e-17, eps_reg_rel=0, svdlim=1e-15, svdnum=None, hermitian=False, use_own=True, closing=None, center_pinhole=None, verbose=True, svd_stat=False):
     '''
     Calculates the unmixing matrix. Assums pi-rotational-symmetry around origin (=conjugate), because PSF is real and only shifted laterally. No aberrations etc. Hence, only half of the OTF-support along z is calculated.
 
@@ -312,15 +318,17 @@ def unmix_matrix(otf, mode='rft', eps_mask=5e-4, eps_reg=1e-17, svdlim=1e-15, sv
     if svdnum is None:
         max_sv_pos = np.unravel_index(np.argmax(proj_mask.flatten()), proj_mask.shape)
         _, outdict_pre = pinv_unmix(otf[:, :, max_sv_pos[0], max_sv_pos[1]],
-                                    rcond=svdlim, svdnum=None, eps_reg=eps_reg, use_own=use_own)
-        svdnum = len(outdict_pre['s'][outdict_pre['s'] > 0])
+                                    svdlim=svdlim, svdnum=None, eps_reg=eps_reg, eps_reg_rel=eps_reg_rel, use_own=use_own)
+        svdnum = len(outdict_pre['s_full'][outdict_pre['s'] > 0])
+        svd_range = unmix_svd_stat(
+            svd_range, outdict_pre['s_full'][outdict_pre['s'] > 0], outdict_pre['eps_reg'], max_sv_pos[0], max_sv_pos[1])
 
         # loop over all kx,ky
     for kk in range(otf_unmix.shape[-2]):
         for jj in range(otf_unmix.shape[-1]):
             if my_mask[:, kk, jj].any():
                 otf_unmix[zoff[kk, jj]:zoff[kk, jj]+proj_mask[kk, jj], :, kk, jj], outdict = pinv_unmix(
-                    otf[:, zoff[kk, jj]:zoff[kk, jj]+proj_mask[kk, jj], kk, jj], rcond=svdlim, svdnum=svdnum, eps_reg=eps_reg, use_own=use_own)
+                    otf[:, zoff[kk, jj]:zoff[kk, jj]+proj_mask[kk, jj], kk, jj], svdlim=svdlim, svdnum=svdnum, eps_reg=eps_reg, eps_reg_rel=eps_reg_rel, use_own=use_own)
                 s_lim = outdict['s'][outdict['s'] > 0]
                 svd_lim_counter[zoff[kk, jj]:zoff[kk, jj]+proj_mask[kk, jj],
                                 :, kk, jj] = len(s_lim)
@@ -330,7 +338,7 @@ def unmix_matrix(otf, mode='rft', eps_mask=5e-4, eps_reg=1e-17, svdlim=1e-15, sv
                 # gather some unmixing statistics
                 if svd_stat:
                     svd_range = unmix_svd_stat(
-                        svd_range, outdict['s_full'][outdict['s'] > 0], kk, jj)
+                        svd_range, outdict['s_full'][outdict['s'] > 0], outdict['eps_reg'], kk, jj)
 
                 # otf[:, zoff[kk, jj]:zoff[kk, jj]+proj_mask[kk, jj], kk, jj] #otf[:, :,90,90]
     otf_unmix = nip.image(otf_unmix)
@@ -346,7 +354,7 @@ def unmix_matrix(otf, mode='rft', eps_mask=5e-4, eps_reg=1e-17, svdlim=1e-15, sv
     svd_counter_dict = {'svd_counter': svd_counter,
                         'svd_lim_counter': svd_lim_counter, 'svd_range': svd_range}
 
-    if verbose:
+    if verbose and svd_stat:
         unmix_svdstat_pp(svd_range)
 
     # done? otf_unmix_full

MicroPy/transformations.py
@@ -6,6 +6,8 @@ from scipy.fftpack import dct
 from pandas import DataFrame
 import NanoImagingPack as nip
 
+from .utility import midVallist
+
 # %%
 # ------------------------------------------------------------------
 #               FOURIER (-like) TRAFOS
@@ -252,11 +254,31 @@ def get_cross_correlations(imlist_rows, imlist_cols, rows=None, cols=None, rlate
     return ncc_pd, ncc_list_latex
 
 
-def energy_regain(recon, groundtruth, atol=1e-8, snorm=True, use_indiv_abs=False):
+def noise_normalize(im, mode='fft'):
+    '''
+    Divides Fourier-Image by center frequency. 
+    '''
+    # find center frequency
+    if mode == 'fft':
+        im_norm = 1/midVallist(im, dims=np.arange(im.ndim), keepdims=True)
+    elif mode == 'rft':
+        im_norm = 1/im.flatten()[0]
+    else:
+        print("Chosen Method unknown hence avaiding normalization.")
+        im_norm = 1
+
+    # norm
+    im = im*im_norm
+
+    # done?
+    return im
+
+
+def energy_regain(recon, groundtruth, atol=1e-20, snorm=True, use_indiv_abs=False, mode='fft'):
     # make sure distance between two images cannot be bigger than 1
     if snorm:
-        recon = recon/np.sum(recon, keepdims=True)
-        groundtruth = groundtruth/np.sum(groundtruth, keepdims=True)
+        recon = noise_normalize(recon, mode=mode)
+        groundtruth = noise_normalize(groundtruth, mode=mode)
 
     # validmask
     nom = np.abs(recon)-np.abs(groundtruth) if use_indiv_abs else np.abs(recon - groundtruth)

MicroPy/utility.py
@@ -4,7 +4,7 @@
 	@author René Lachmann
 	@email herr.rene.richter@gmail.com
 	@create date 2019 11:53:25
-	@modify date 2021-09-28 15:38:32
+	@modify date 2021-11-08 13:59:07
 	@desc Utility package
 
 ---------------------------------------------------------------------------------------------------
@@ -113,9 +113,10 @@ def noise_poisson_gauss(im, phot, mu, sigma):
     return im
 
 
-def noise_normalize(im, phot, norm='mean'):
+def factor_normalize(im, phot, norm='mean'):
     '''
     Normalizes the image so that proper noise can be applied.
+    old_name ('noise_normalize') was misleading. 
     '''
     if norm == 'mean':
         norm = np.mean(im, axis=(-2, -1)) / phot
@@ -922,7 +923,7 @@ def transpose_arbitrary(imstack: np.ndarray, idx_startpos: list = [-2, -1], idx_
     return im
 
 
-def subslice_arbitrary(im, roi):
+def subslice_arbitrary(im, slice_range, axes):
     """Select arbitrary subslice.
     For now: creates a copy of the input object. 
 
@@ -930,9 +931,11 @@ def subslice_arbitrary(im, roi):
     ----------
     im : image
         N dimensional image
-    roi : array
+    slice_range : array
         list to be used for subslicing. Shape has to be of the kind: 
         [dim,start,stop] per dimension, see example
+    axes : array
+        axes to be used
 
     Returns
     -------
@@ -942,8 +945,9 @@ def subslice_arbitrary(im, roi):
     Example
     -------
     >>> im = np.reshape(np.ones([2*3*4*5]),[2,3,4,5])
-    >>> roi = [[1,1,3],[2,0,3],[3,0,3]]
-    >>> a = selectROI(im,roi)
+    >>> slice_range = [[1,1,3],[2,0,3],[3,0,3]]
+    >>> axes = [-3,-2,-1]
+    >>> a = subslice_arbitrary(im,slice_range,axes)
     >>> print(f"im={im.shape}\na={a.shape}")
     im=(2, 3, 4, 5)
     a=(2, 2, 3, 3)
@@ -953,12 +957,12 @@ def subslice_arbitrary(im, roi):
     transpose_arbitrary
     """
     #roi = [dim,start,stop]
-    for _, roipos in enumerate(roi):
+    for m, ax in enumerate(axes):
         im = transpose_arbitrary(
-            im, idx_startpos=roipos[0], idx_endpos=0, direction='forward')
-        im = im[roipos[1]:roipos[2]]
+            im, idx_startpos=slice_range[m][0], idx_endpos=0, direction='forward')
+        im = im[slice_range[m][1]:slice_range[m][2]]
         im = transpose_arbitrary(
-            im, idx_startpos=roipos[0], idx_endpos=0, direction='backward')
+            im, idx_startpos=slice_range[m][0], idx_endpos=0, direction='backward')
     return im
 
 
@@ -1237,6 +1241,26 @@ def create_value_on_dimpos(dims_total, axes=[-2, -1], scaling=[0.5, 0.5]):
     for m in range(len(axes)):
         res[axes[m]] = scaling[m]
     return res
+
+
+def midIndexlist(iml, dims):
+    # sanity
+    dims = np.mod(dims, iml.ndim)
+
+    # preparation
+    im_centers = np.array(iml.shape)//2
+    idx_list = np.reshape(np.arange(np.prod(iml.shape)), iml.shape)
+
+    # find idx
+    dim_sel = []
+    for dim in range(iml.ndim):
+        if dim in dims:
+            dim_sel.append(slice(im_centers[dim], im_centers[dim]+1))
+        else:
+            dim_sel.append(slice(0, iml.shape[dim], 1))
+
+    # done?
+    return idx_list[dim_sel].flatten(), dim_sel
 
 
 def midVallist(iml, dims, method='iter', keepdims=False):
@@ -1774,7 +1798,7 @@ def time_me(fcall, repeats=1000, averages=100):
     >>>     return 2*x+y
     >>> def myminus(x,y):
     >>>     return 2*x-np.min([x,y])
-    >>> print(mipy.time_me(lambda: mysq(mysum(10,myminus(5,4)))repeats=1000)[1][0])
+    >>> print(mipy.time_me(lambda: mysq(mysum(10,myminus(5,4))),repeats=1000)[1][0])
     9.433002560399473e-06
 
     See Also